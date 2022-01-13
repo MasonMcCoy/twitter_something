2dc9f45e339d22fbf0a0ffc7bc7a5ea0643d8b46
Mason McCoy <mmasonmccoy@gmail.com>
2022-01-13 16:46:41 -0600

    Got Twitter API to return data. Retrieve ID and associated tweets. Some light binning.

--- app.py
+++ app.py
@@ -5,16 +5,37 @@ Created on Fri Jan  7 12:49:31 2022
 @author: mmaso
 """
 
+
 import tweepy
+import requests
 from config import api_key, api_secret_key, bearer_token
 
-# print(api_key)
-
-
-# auth = tweepy.OAuthHandler(bearer_token, api_key, api_secret_key)
-client = tweepy.Client(bearer_token, api_key, api_secret_key)
+client = tweepy.Client(bearer_token, api_key, api_secret_key, return_type= requests.Response)
 
 username = 'WillyMacShow'
-user = client.get_user(username = username)
 
-print(user)
+user = client.get_user(username = username)
+user_json = user.json()
+
+user_id = user_json['data']['id']
+
+print(f"{username}'s user ID is {user_id}\n")
+
+user_tweets = client.get_users_tweets(user_id, expansions = ['author_id', 'referenced_tweets.id'], max_results = 5)
+user_tweets_json = user_tweets.json()
+
+user_tweets_list = user_tweets_json['data']
+#print(user_tweets_list)
+
+print(f"{username} has made the following Tweets:\n")
+
+for tweet in user_tweets_list:
+    # print(f"Author ID: {tweet['author_id']}")
+    print(f"Tweet ID: {tweet['id']}")
+    
+    try:
+        print(f"{tweet['referenced_tweets'][0]['type']}: {tweet['referenced_tweets'][0]['id']}")
+    except:
+        print('tweet') # Not sure about the naming convention here
+    
+    print(f"{tweet['text']}\n")
