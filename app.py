54453f6414658a4de79c4145676ad979b5197894
Mason McCoy <mmasonmccoy@gmail.com>
2022-02-01 16:02:27 -0600

    Refactored code to be more in line with principles of OOP. Added reply count. Able to reproduce tweets with comment threads.

--- app.py
+++ app.py
@@ -5,10 +5,18 @@ Created on Fri Jan  7 12:49:31 2022
 @author: mmaso
 
 TODO:
+    Pagination for replies? get_recent_tweets
     Scale username up. Test list of usernames.
     Inconsistencies between Twiiter app and API results, i.e. likes not matching for tweets
     Where is this all going? DB? Email?
     What does it mean to be ratioed? Enough likes to be relevant, but lower than usual? Compare to average?
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  1 11:46:42 2022
+
+@author: mmaso
 """
 
 import tweepy
@@ -16,77 +24,113 @@ import requests
 import datetime as dt
 from config import api_key, api_secret_key, bearer_token
 
-username = 'WillyMacShow' # This will be a list, potential user input?
+# Username(s) to be input into API request(s)
+#username = 'WillyMacShow' # Potential user input?
 
 # API v2 Client
 client = tweepy.Client(bearer_token, api_key, api_secret_key, return_type= requests.Response)
 
-# API request for User Lookup
-user = client.get_user(username = username).json()
+# One Day    
+day_1 = dt.datetime.now() - dt.timedelta(days = 1) # Time variable
 
-# Twitter ID
-user_id = user['data']['id']
+def time_to_rfc3339(time_variable):
+    # Adjusts input time to rfc3339 format for API request
+    str_year3339 = time_variable.isoformat().split('T')
+    
+    return str_year3339[0] + 'T00:00:00Z'
 
-print(f"{username}'s user ID is {user_id}\n")
 
-# Year-to-Date    
-ytd_time = dt.datetime.now() - dt.timedelta(days = 1) # Time variable
+def rfc3339_to_date(rfc3339_variable):
+    # Splits returned 'created_at' field into date and time, converts to readable date
+    date, time = rfc3339_variable.split('T')
+    
+    return date
 
-# Adjusts YTD to rfc3339 format
-str_year3339 = (ytd_time.isoformat())
-ymd_str_year3339 = str_year3339.split('T')
-adj_3339 = ymd_str_year3339[0] + 'T00:00:00Z'
+def rfc3339_to_time(rfc3339_variable):
+    # Splits returned 'created_at' field into date and time, converts to readable time
+    date, time = rfc3339_variable.split('T')
+    time, zone = time.split('.')
+    
+    return time
 
-# API request for Tweets using Twitter ID
-user_tweets = client.get_users_tweets(user_id, expansions = ['author_id', 'referenced_tweets.id'], start_time = adj_3339, tweet_fields = ['created_at']).json()
-# print(user_tweets) # use this to navigate request structure when adding new fields
 
-print(f"{username} has made the following Tweets:\n")
+class TwitterUser:
+    def __init__(self, username):
+        self.username = username
+        self.tweets = []
+        
+    def getUserID(self):
+        user = client.get_user(username = self.username).json()
+        user_id = user['data']['id']
+        
+        return user_id
+    
+    def getTweets(self, pagination = False, token = ''):
+        
+        if pagination is True:
+            twitter_response = client.get_users_tweets(self.getUserID(), expansions = ['referenced_tweets.id'], pagination_token = token, start_time = time_to_rfc3339(day_1), tweet_fields = ['created_at']).json()
+        
+        else:
+            twitter_response = client.get_users_tweets(self.getUserID(), expansions = ['referenced_tweets.id'], start_time = time_to_rfc3339(day_1), tweet_fields = ['created_at']).json()
+        
+        for tweet in twitter_response['data']:
+            self.tweets.append(tweet)
+        
+        if 'next_token' in twitter_response['meta']:
+            '''YOU NEED TO PAGINATE'''
+                
+            token = twitter_response['meta']['next_token']
+            self.getTweets(pagination = True, token = token)
+                
+        else:
+            '''NO PAGINATION/PAGINATION COMPLETE'''
+            for tweet in self.tweets:
+                tweet_likes = client.get_liking_users(tweet['id']).json()
+                tweet_rt = client.get_retweeters(tweet['id']).json()
+                tweet_replies = client.search_recent_tweets(f"conversation_id:{tweet['id']}").json()
+                
+                tweet['likes'] = tweet_likes['meta']['result_count']
+                tweet['retweets'] = tweet_rt['meta']['result_count']
+                tweet['replies'] = tweet_replies['meta']['result_count']
+    
+    def formatTweets(self):
+        for tweet in self.tweets:
+            print(f"Tweet ID: {tweet['id']}")
+            print(f"{rfc3339_to_date(tweet['created_at'])} at {rfc3339_to_time(tweet['created_at'])} GMT")
+            
+            try:
+                print(f"{tweet['referenced_tweets'][0]['type']} Twitter ID: {tweet['referenced_tweets'][0]['id']}")
+            except:
+                print('original tweet')
+            
+            print(tweet['text'], '\n')
+            print(f"Replies: {tweet['replies']}          Retweets: {tweet['retweets']}          Likes: {tweet['likes']}\n")
+        
+        print(f"Total Tweets Requested: {len(self.tweets)}")
+        
+    def getDrama(self):
+        """
+        look at replies, find likes, compare reply likes to tweet likes
+        """
+        for tweet in self.tweets:
+            replies = client.search_recent_tweets(f"conversation_id:{tweet['id']}").json()
+            
+            print(tweet['text'])
+            
+            try:
+                for reply in replies['data']:
+                    print(f"# {reply['text']}")
+            except:
+                print("No replies :(")
+                
+            print('\n')
 
-tweet_likes = []
-tweet_retweets = []
+    
+    def __repr__(self):
+        return f"{self.username}'s Twitter ID is {self.getUserID()}\n\nThey have made the following Tweets in the past 24 hours:\n{self.tweets}"
+    
+wms = TwitterUser('WillyMacShow')
 
-for tweet in user_tweets['data']:
-    # print(f"Author ID: {tweet['author_id']}")
-    
-    # API request for Tweet Likes
-    tweet_likes_user = client.get_liking_users(tweet['id']).json() #Tweet ID
-    
-    # API request for Tweet Retweets
-    tweet_rt_user = client.get_retweeters(tweet['id']).json() #Tweet ID
-    
-    # Tweet ID
-    print(f"Tweet ID: {tweet['id']}")
-    
-    # Tweet Timestamp
-    print(f"Created at {tweet['created_at']}")
-    
-    # Tweet Type (retweeted, quoted, or replied_to, original tweet)
-    try:
-        ref_tweet_id = str(tweet['referenced_tweets'][0]['id'])
-        user = client.get_tweet(id = ref_tweet_id, expansions = ['author_id']).json()
-        print(f"{tweet['referenced_tweets'][0]['type']}: {user['includes']['users'][0]['username']}") # 'referenced_tweets' key does not appear for original tweets
-    except:
-        print('original tweet') # Not sure about the naming convention here
-    
-    # Tweet text
-    print(f"{tweet['text']}")
-    
-    # Tweet Like and Retweet Counts
-    likes = tweet_likes_user['meta']['result_count']
-    retweet = tweet_rt_user['meta']['result_count']
-    
-    tweet_likes.append(likes)
-    tweet_retweets.append(retweet)
-    print(f"Likes: {likes}     Retweets: {retweet}\n")
-    
-    # Retweeter usernames and Twitter IDs (where applicable)
-    try:
-        print(f"Retweeted by:\n{tweet_rt_user['data'][0]['username']}: {tweet_rt_user['data'][0]['id']}\n") # 'data' key does not appear in dicts w/o rts
-    except:
-        pass
-
-# Descriptive stats on selected Tweets
-print(f"Total Tweets returned: {len(user_tweets['data'])}")
-print(f"Total Likes: {sum(tweet_likes)}      Likes/Tweet: {sum(tweet_likes)/len(user_tweets['data'])}")
-print(f"Total Retweets: {sum(tweet_retweets)}     Retweets/Tweet: {sum(tweet_retweets)/len(user_tweets['data'])}")
+#print(wms.getUserID())
+#print(wms.getTweets())
+#print(wms)
